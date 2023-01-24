82bbb19f1948f353d561b5fe0d6718d475b4d3b1
Bonface wambugu Maina <44479507+stockmn@users.noreply.github.com>
2023-01-24 17:27:36 +0300

    Final commit

--- cypress/e2e/logins.cy.js
+++ cypress/e2e/logins.cy.js
@@ -1,16 +1,17 @@
 
 import 'cypress-xpath';
-Cypress._.times(2, (k) => {
+Cypress._.times(4, (k) => {
     it(`Login ${k + 1} / 2`, () => {
       cy.visit('https://accounts.jambopay.com/v2/account/login')
       cy.contains("Phone Number").type("0708901215")
       cy.contains("Password").type("123456")
       cy.contains("LOGIN").click()
-      //cy.contains("csrfmiddlewaretoken")
-      //if you are able to target the value of the csrfmiddlewaretoken in the 1 st loop you log the value of the tokenfor the second loop of test you
-      //target the value of the csrfmiddlewaretoken and log the value somewhere 
-      //then test if the value of ist loop where k=1 csrfmiddlewaretoken is not equal to the value of 2nd loopwhere k=2
-      //if k=1 csrfmiddlewaretoken != k=2 csrfmiddlewaretoken value the test is passed
+      /*cy.contains("csrfmiddlewaretoken")
+      if you are able to target the value of the csrfmiddlewaretoken in the 1 st loop where k ==1, log the value of the token   With valuable k1=csrfmiddlewaretoken VALUE .
+      for the second loop where k ==2  of test you
+      target the value of the csrfmiddlewaretoken and log the csrfmiddlewaretoken value somewhere with K2= csrfmiddlewaretoken VALUE
+      then test if K1!=K2 test passed else test failed
+      if k=1 csrfmiddlewaretoken != k=2 csrfmiddlewaretoken value the test is passed */
      
       
     })
