a796b58a85a623be3b55b8768e03fc78753a733a
Bonface wambugu Maina <44479507+stockmn@users.noreply.github.com>
2023-01-24 17:17:01 +0300

    third

--- cypress/e2e/logins.cy.js
+++ cypress/e2e/logins.cy.js
@@ -6,7 +6,11 @@ Cypress._.times(2, (k) => {
       cy.contains("Phone Number").type("0708901215")
       cy.contains("Password").type("123456")
       cy.contains("LOGIN").click()
-      cy.xpath("/html/body/div/div/div[2]/form/input")
+      //cy.contains("csrfmiddlewaretoken")
+      //if you are able to target the value of the csrfmiddlewaretoken in the 1 st loop you log the value of the tokenfor the second loop of test you
+      //target the value of the csrfmiddlewaretoken and log the value somewhere 
+      //then test if the value of ist loop where k=1 csrfmiddlewaretoken is not equal to the value of 2nd loopwhere k=2
+      //if k=1 csrfmiddlewaretoken != k=2 csrfmiddlewaretoken value the test is passed
      
       
     })
